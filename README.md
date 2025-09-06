652455d0dd4e07652cf24c5ed944a37c15605eaa
Subhasreesubham <subhasubhasreee@gmail.com>
2025-09-06 23:58:57 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nm digital portfolio 
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/subhasreee-subha/pen/pvjGxQo](https://codepen.io/subhasreee-subha/pen/pvjGxQo).
+
---
+++ script.js
@@ -0,0 +1,39 @@
+// Typewriter Effect
+
+const text = "B.Sc CS Student |
+
+Aspiring Software Developer | Future
+
+Zoho Engineer";
+
+let i = 0;
+
+function typeWriter() {
+
+if (i < text.length) {
+
+document.getElementByl
+
+d("tagline").innerHTML +=
+
+text.charAt(i);
+
+i++;
+
+setTimeout(typeWriter, 80);
+
+}
+
+}
+
+typeWriter();
+
+// Button click alert
+
+function sayHello() {
+
+alert("Hello! Thanks for visiting my
+
+portfolio");
+
+}
